a4e4debc5a8f4cb0cdb1ed89dba1a1ea1aeb79dc
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-08-09 13:19:09 +0200

    pusher installed, public repo created

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+var Pusher = require('pusher');
+
+var channels_client = new Pusher({
+  appId: '839971',
+  key: '2086a575e597e3d798f0',
+  secret: '5a0318fa4ea4dcbc3cbe',
+  cluster: 'eu',
+  encrypted: true
+});
+
+channels_client.trigger('my-channel', 'my-event', {
+  "message": "hello world"
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World')
